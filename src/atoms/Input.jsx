672f28f73a03b41d242e2be0ffe99c452261fdc6
Vitalii Shapoval <vitalii.sh@walnut.io>
2024-06-04 18:22:44 +0300

    feat: add validation message markup

--- src/atoms/Input.jsx
+++ src/atoms/Input.jsx
@@ -2,6 +2,17 @@ import { useState, useEffect } from 'react'
 
 import "./Input.css"
 
+export const ErrorMessage = ({ className = "", isError=false, isSuccess=false, text }) => {
+    const classNames = [
+        isError ? "message-error" : "",
+        isSuccess ? "message-success" : "",
+        className,
+    ].join("")
+    return (
+        <p className={classNames}>{text}</p>
+    )
+}
+
 const Input = ({ className = "", type, value, placeholder = "", isError = false, isDisabled = false, isSuccess = false, icon = null }) => {
     const [isFocused, setIsFocused] = useState(false)
     const classNames = [
--- src/pages/SignupForm.jsx
+++ src/pages/SignupForm.jsx
@@ -3,13 +3,13 @@ import { useState } from 'react'
 import "./SignupForm.css"
 
 import IconEyeCrossed from '../icons/IconEyeCrossed'
-import Input from '../atoms/Input'
+import Input, { ErrorMessage } from '../atoms/Input'
 
 const emailRegex = /^(([^<>()\[\]\.,;:\s@"]+(\.[^<>()\[\]\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
 const SignupForm = ({ className = "" }) => {
     const [isPasswordShown, setIsPasswordShown] = useState(false)
-1
+    1
     const handleIsPasswordShown = e => {
         e.preventDefault()
         setIsPasswordShown(!isPasswordShown)
@@ -19,13 +19,24 @@ const SignupForm = ({ className = "" }) => {
         <div className={`form-wrapper ${className}`}>
             <h1 className='form-header'>Sign up</h1>
             <form>
-                <Input value="something" type="email" className="form-input-wrapper" isError={false} isDisabled={false} isSuccess={false} />
+                <Input
+                    value="something"
+                    type="email"
+                    className="form-input-wrapper"
+                    isError={false}
+                    isDisabled={false}
+                    isSuccess={false} />
                 <Input
                     value="pass"
                     type={isPasswordShown ? "text" : "password"}
                     className="form-input-wrapper"
                     icon={<IconEyeCrossed className="icon-password" onClick={handleIsPasswordShown} />}
                 />
+                <div className='form-errors-container'>
+                    <ErrorMessage text="Has at least 8 characters (no spaces)" isError={true} />
+                    <ErrorMessage text="Uppercase and lowercase letters" />
+                    <ErrorMessage text="1 digit minimum" isSuccess={true} />
+                </div>
             </form>
         </div>
     )
