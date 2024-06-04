4d229a6ae6a0523306792efb518ed03ea944f3b1
Vitalii Shapoval <vitalii.sh@walnut.io>
2024-06-04 18:07:03 +0300

    feat: add show/hidde password toggle

--- src/atoms/Input.jsx
+++ src/atoms/Input.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 
 import "./Input.css"
 
-const Input = ({ type, value, placeholder = "", isError = false, isDisabled = false, isSuccess = false }) => {
+const Input = ({ className = "", type, value, placeholder = "", isError = false, isDisabled = false, isSuccess = false, icon = null }) => {
     const [isFocused, setIsFocused] = useState(false)
     const classNames = [
         "input",
@@ -21,14 +21,18 @@ const Input = ({ type, value, placeholder = "", isError = false, isDisabled = fa
     }
 
     return (
-        <input className={classNames}
-            type={type}
-            value={value}
-            placeholder={placeholder}
-            onFocus={handleFocus}
-            onBlur={handleBlur}
-            disabled={isDisabled}
-        />
+        <div className={`input-container ${className}`}>
+            <input className={classNames}
+                type={type}
+                value={value}
+                placeholder={placeholder}
+                onFocus={handleFocus}
+                onBlur={handleBlur}
+                disabled={isDisabled}
+            />
+            
+            {icon}
+        </div>
     )
 }
 
--- src/pages/SignupForm.jsx
+++ src/pages/SignupForm.jsx
@@ -1,15 +1,31 @@
+import { useState } from 'react'
+
 import "./SignupForm.css"
 
+import IconEyeCrossed from '../icons/IconEyeCrossed'
 import Input from '../atoms/Input'
 
 const emailRegex = /^(([^<>()\[\]\.,;:\s@"]+(\.[^<>()\[\]\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
-const SignupForm = ({ className="" }) => {
+const SignupForm = ({ className = "" }) => {
+    const [isPasswordShown, setIsPasswordShown] = useState(false)
+1
+    const handleIsPasswordShown = e => {
+        e.preventDefault()
+        setIsPasswordShown(!isPasswordShown)
+    }
+
     return (
         <div className={`form-wrapper ${className}`}>
             <h1 className='form-header'>Sign up</h1>
             <form>
-                <Input value="something" type="email" isError={false} isDisabled={false} isSuccess={false} />
+                <Input value="something" type="email" className="form-input-wrapper" isError={false} isDisabled={false} isSuccess={false} />
+                <Input
+                    value="pass"
+                    type={isPasswordShown ? "text" : "password"}
+                    className="form-input-wrapper"
+                    icon={<IconEyeCrossed className="icon-password" onClick={handleIsPasswordShown} />}
+                />
             </form>
         </div>
     )
